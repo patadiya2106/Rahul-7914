ed0d9b87c83070a0c6bc7e2adb2ffce255a87c92
patadiya2106 <rahulpatadiya07@gmail.com>
2024-11-19 19:07:49 +0530

    output-19-11-2024

---
+++ day-1.js
@@ -0,0 +1,28 @@
+
+    // 1. console.log()
+    // 2. alert()
+    // 3. document.write()
+    // 4. DOM
+
+
+// 1. console.log()
+
+// console.log("HELLO WORLD")
+
+// console.warn("YOU ARE IN DANGER AREA")
+
+// console.error("WRONG PLZ TRY AGAIN")
+
+// console.log(9+4)
+
+// 2. alert()
+
+// alert("HELLO WELCOME TO THE NEW TAB")
+
+// 3. document.write()
+
+// document.write("JavaScript is very hard language")
+
+// 4. DOM
+
+document.getElementById("para").innerHTML="hello welcome to the world of javascript"
