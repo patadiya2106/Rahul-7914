0e881df6cfcc10834cea63e16425cef7e5652068
patadiya2106 <rahulpatadiya07@gmail.com>
2024-11-20 18:26:59 +0530

    Add files via upload
    VARIABLES - 20-11-2024

---
+++ variable.js
@@ -0,0 +1,38 @@
+// Variables is like a container , too store information or data
+
+// initilization
+
+// var r;
+
+// let a;
+
+// const PI = 3.14; // delecaration
+
+// r = 10;
+// console.log(r);
+
+
+// Data Types => premetive or non premetive
+
+// // premetive =>
+//     -Number
+//     - String
+//     - null
+//     - undefined
+//     - Boolean
+//     - Symbol
+//     - BigInt
+    
+// // Non Premetive
+//     - object
+
+
+let num = 30;
+console.log(num);
+console.log(typeof num);
+
+
+
+    
+    
+
