fcfe6ce0677f56907a45bed08d2d58f1347dea3e
patadiya2106 <rahulpatadiya07@gmail.com>
2024-11-21 18:58:56 +0530

    21-11-2024

---
+++ operator.js
@@ -0,0 +1,64 @@
+// OPERTORS
+
+// airthmetical opertors : + - * / % **
+// assignment opertors : += -= *= /= %= **=
+// conditional opertors : < <= > >= == != === !==
+// logical opertors : && || !
+
+
+
+// AIRTHMETICAL OPERTORS
+
+// let r = 9; let p = 7;
+
+
+// console.log(r + p);
+// console.log(r - p);
+// console.log(r * p);
+// console.log(r / p);
+// console.log(r % p);
+// console.log(r ** p);
+
+
+// ASSIGNMENT OPERTORS
+
+// let x = 22; let y = 35;
+
+// console.log(x += y);
+// console.log(x -= y);
+// console.log(x *= y);
+// console.log(x /= y);
+// console.log(x %= y);
+// console.log(x **= y);
+
+
+// conditional opertors
+
+// let p = 17; let q = 19;
+
+// console.log(p > q);
+// console.log(p >= q);
+// console.log(p < q);
+// console.log(p <= q);
+// console.log(p == q);
+// console.log(p != q);
+// console.log(p === q);
+// console.log(p !== q);
+
+// conditional opertors
+
+// let s = 0, t = 1;
+
+// console.log(s && t);
+// console.log(s && t);
+// console.log(!(s > 0 && t > 0));
+// console.log(s || t);
+// console.log(s || t);
+// console.log(!(s > 0 || t > 0));
+
+
+
+
+
+
+
