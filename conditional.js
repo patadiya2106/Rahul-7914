4e39bfffb5a7cb766d511840fd0c3c3dfbcdb5c1
patadiya2106 <rahulpatadiya07@gmail.com>
2024-11-22 18:58:38 +0530

    22-11-2024

---
+++ conditional.js
@@ -0,0 +1,71 @@
+/* Conditional statement :- if else , nested , laddder statement , switch case , ternory 
+
+
+if (condition){
+    statement
+}
+else{
+    statement
+}
+
+*/
+
+// FIND SMALL NUMBER
+
+// let x = 20 , y = 39;
+
+// if (x < y) {
+//     console.log("Small number is: ", x)
+// }
+// else {
+//     console.log("small number is : " , y)
+// }
+
+
+// ternory opertor
+
+// condition ? true : false
+
+// let a = 25, b = 50;
+
+// a > b ? console.log("big number is : ", a) : console.log("big number is : ", b);
+
+// console.log("big number is : ", a > b ? a : b);
+
+// Swapping number
+
+// 1.with third varibles
+// 2.without third varibles
+
+    // i) Third Variable
+
+    // let a = 77, b = 88, c
+    // console.log("Before Swapping --->");
+    // console.log("A ----", a);
+    // console.log("B ------->", b);
+
+    // c = a;
+    // a = b;
+    // b = c;
+
+    // console.log("After Swapping");
+    // console.log("A ------->", a);
+    // console.log("B ------->", b);
+
+    // // ii) Without Third Variable
+
+    // let x = 22, y = 20;
+    // console.log("Before Swapping ");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+    // x = x + y;
+    // y = x - y;
+    // x = x - y;
+
+    // console.log("After Swapping");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+
+
