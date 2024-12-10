e0173bf266c60e326ffe13968c236c129b3b03be
patadiya2106 <rahulpatadiya07@gmail.com>
2024-12-10 18:55:31 +0530

    10-12-2024

---
+++ recusrion.js
@@ -0,0 +1,48 @@
+// Recusrion function ===> A function call itself again and again
+
+
+
+// function helloWorld(r) {
+//     console.log("Inside function Called", r);
+//     if (r > 15) {
+//         helloWorld(--r);
+//     }
+//     console.log("Function Finished.....", r);
+// }
+
+// helloWorld(20);
+
+// ------------------------------------------------------------------------
+
+// factorial Number
+
+// function factorial(j) {
+//     if (j == 1) {
+//         return 1;
+//     } else {
+//         return j * (factorial (j - 1));
+//     }
+// }
+
+// console.log(factorial(10));
+
+// ------------------------------------------------------------
+
+// fabonacci Series
+
+function fab(r, s, t) {
+    for(let i = )
+    if (r == 0) {
+        console.log(r)
+    }
+    else if (r == 1) {
+        console.log(t)
+    }
+    else {
+        return  t = r + s;
+                 r = s;
+                s = t;
+
+    }
+}
+console.log(fab(3));
