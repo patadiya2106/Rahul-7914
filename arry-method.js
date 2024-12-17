524657856fd2d0ca1f90747deaeac17b8c4a9440
patadiya2106 <rahulpatadiya07@gmail.com>
2024-12-17 18:49:04 +0530

    16-12-2024

---
+++ arry-method.js
@@ -0,0 +1,18 @@
+// Array => store a mulliple value in single varibles....
+
+let arry = [10, 20, 30, 40, 50, 60]
+
+// console.log(arry);
+
+// console.log(arry[4]) index to fid a value
+
+// arry[20] = 200;
+// arry[55] = 3021;
+// arry[85] = 300;
+// console.log(arry);
+
+// arry[4] = "Smitbhai Don no.=1"
+// arry[5] = "jaypawarbhai don no.=2"
+// console.log(arry)
+
+// console.log(typeof arry)     type cheack 
