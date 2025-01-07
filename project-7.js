5cca0459ce66c4ee74c2e751e71da8a23df32582
Rahul Patadiya <rahulpatadiya07@gmail.com>
2025-01-07 15:15:32 +0530

    6-1-2025 Project

--- project-7.js
+++ project-7.js
@@ -1,22 +1,40 @@
 // create a school class and create and faclty method Name , email , contact number
 
 class rnw {
-    constructor(name) {
-        this.name = name;
+    student(name, email, contactNumber) {
+        console.log("Student Details:- ");
+        console.log(`Name: ${name}`);
+        console.log(`Email: ${email}`);
+        console.log(`Contact: ${contactNumber}`);
     }
 
-    student(name, email, contact) {
-        console.log(`Student Name : ${this.name}`)
-        console.log(`Student Email : ${this.email}`)
-        console.log(`Student Contact-number : ${this.contact}`)
+    faculty(name, email, contactNumber) {
+        console.log("Faculty Details:- ");
+        console.log(`Name: ${name}`);
+        console.log(`Email: ${email}`);
+        console.log(`Contact: ${contactNumber}`);
     }
 }
 
-// const facult
+let details = new rnw();
+
+details.student("Rahul Patadiya", "rahulpatadiya07@gmail.com", "8000828245");
+
+details.faculty("Mr.Girish Sir", "girishgondliya50@gmail.com", "98765643210");
+
+// Output : -Student Details:- 
+// Name: Rahul Patadiya
+// Email: rahulpatadiya07@gmail.com
+// Contact: 8000828245
+// Faculty Details:- 
+// Name: Mr.Girish Sir
+// Email: girishgondliya50@gmail.com
+// Contact: 98765643210
+
+// ------------------------------------------------------------------------------------------------------------------------
 
 
 // 2.Create Person class with argument Name ane age
-
 
 class person {
     constructor(name, age) {
@@ -26,9 +44,49 @@ class person {
 }
 class employee extends person {
     hello() {
-        console.log(`MY Name is ${this.name} MY Age is ${this.age}`);
+        console.log(`Name :${this.name} Age : ${this.age}`);
     }
 }
-const detail = new employee("Rahul", "23");
+const detail = new employee("Rahul Patadiya", "23");
 
 detail.hello();
+
+// Output : Name :Rahul Patadiya
+//           Age : 23 
+// Employee class creates a constructor called print a simple message.
+
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// 3.Employee class creates a constructor called print a simple message
+
+class data {
+    constructor(){
+        console.log("Hello sir i learning javascrip!!!.......")
+    }
+}
+
+const storedata = new data()
+
+// Output : Hello sir i learning javascrip!!!.......
+
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// 4.Create a shape class and create a rectangle and circle method find circle........
+
+class shape  {
+    rectangleArea(w, h) {
+        return w * h;
+    }
+    circleArea(r) {
+        return 3.14 * r * r;
+    }
+}
+
+const value = new shape ();
+console.log("Rectangle Area is :", value.rectangleArea(35, 28));
+console.log("Circle Area is :", value.circleArea(46));
+
+// Output :Rectangle Area is : 980
+//          Circle Area is : 6644.24
